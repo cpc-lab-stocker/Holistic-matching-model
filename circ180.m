877d193e01f94f5b3fa6f0d08c9356fb5cdeb8ba
Computational Perception and Cognition Laboratory <37944336+cpc-lab-stocker@users.noreply.github.com>
2022-04-06 13:27:01 -0400

    Add files via upload

--- circ180.m
+++ circ180.m
@@ -1,32 +1,17 @@
-function [ out ] = circ180( in )
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
-% size_in = size(in);
-% for i = 1:size_in(1)
-%     for j = 1:size_in(2)
-%         while in(i,j) <0
-%             in(i,j) = in(i,j)+180;
-%         end
-% 
-%         while in(i,j) >= 180
-%             in(i,j) = in(i,j)-180;
-%         end
-% %         out(i,j) = in(i,j);
-%     end
-% end
-% out = in;
+function [ theta ] = circ180( theta )
+%wrap theta to [0, 180)
+% theta: scaler, vector, or 2D matrix
 
-large = in >= 180;
-small = in < 0;
-while (sum(sum(large))) >0
-    in(large) = in(large)-180;
-    large = in >= 180;
+large = theta >= 180;
+small = theta < 0;
+while (sum(sum(large))) > 0
+    theta(large) = theta(large)-180;
+    large = theta >= 180;
 end
-while (sum(sum(small))) >0
-    in(small) = in(small)+180;
-    small = in < 0;
+while (sum(sum(small))) > 0
+    theta(small) = theta(small)+180;
+    small = theta < 0;
 end
-out = in;
 
 end
 
